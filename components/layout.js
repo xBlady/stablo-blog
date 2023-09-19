84dd96050755d945eaa738dd78154d3067c0bfc0
xBlady <pasierbek.general@gmail.com>
2023-09-19 14:04:03 +0200

    Change _document.js location to /pages root directory

--- components/layout.js
+++ components/layout.js
@@ -18,6 +18,21 @@ export default function Layout(props) {
       <Head>
         <link rel="preconnect" href="https://cdn.sanity.io/" />
         <link rel="dns-prefetch" href="https://cdn.sanity.io//" />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i) {
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-52QVBZ57');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <NextSeo
         title={props.title}
