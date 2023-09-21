fabb88e0f7fc1cc07bb1a81ded5a30d573fe6ae7
xBlady <pasierbek.general@gmail.com>
2023-09-21 20:15:21 +0200

    updated _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import '../public/fonts/Clash-Display.css'; // Update the path accordingly
+
 
 class MyDocument extends Document {
   render() {
