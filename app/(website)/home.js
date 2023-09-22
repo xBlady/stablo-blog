0453d5b070aae7e0c3031d0b88ee48eddfe1b672
xBlady <pasierbek.general@gmail.com>
2023-09-22 20:47:12 +0200

    updated import path for aboutsection

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Container from "@/components/container";
 import PostList from "@/components/postlist";
-import AboutSection from "@/components/aboutSection";  // Updated import path
+import AboutSection from "@/components/aboutsection";  // Updated import path
 
 export default function Post({ posts }) {
   return (
