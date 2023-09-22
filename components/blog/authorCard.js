b6cc15f7a7687a80bad52ed58810e66094d84b13
xBlady <pasierbek.general@gmail.com>
2023-09-22 21:35:10 +0200

    authorcard changed corners radius

--- components/blog/authorCard.js
+++ components/blog/authorCard.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function AuthorCard({ author }) {
   const imageProps = author?.image ? urlForImage(author.image) : null;
   return (
-    <div className="mt-3 rounded-2xl bg-gray-50 px-8 py-8 text-gray-500 dark:bg-gray-900 dark:text-gray-400">
+    <div className="mt-3 bg-gray-50 px-8 py-8 text-gray-500 dark:bg-gray-900 dark:text-gray-400">
       <div className="flex flex-wrap items-start sm:flex-nowrap sm:space-x-6">
         <div className="relative mt-1 h-24 w-24 flex-shrink-0 ">
           {imageProps && (
