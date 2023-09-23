0240bdc09d49d252fe4c4305c9cf8238199e71ec
xBlady <pasierbek.general@gmail.com>
2023-09-23 22:00:11 +0200

    button

--- components/navbar.js
+++ components/navbar.js
@@ -70,7 +70,7 @@ export default function Navbar(props) {
                   </Link>
                   <Disclosure.Button
                     aria-label="Toggle Menu"
-                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
+                    className="ml-auto h-full rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
                     <svg
                       className="h-6 w-6 fill-current"
                       xmlns="http://www.w3.org/2000/svg"
